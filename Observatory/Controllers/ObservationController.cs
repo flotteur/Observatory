c37cf82e139fdb6dab75d5881dc67033f9776a54
Flotteur <flotteur@gmail.com>
2015-05-07 22:18:30 -0400

    Created CRUD interface

---
+++ Observatory/Controllers/ObservationController.cs
@@ -0,0 +1,54 @@
+﻿using Observatory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Observatory.Controllers
+{
+    public class ObservationController : ApiController
+    {
+        /// <summary>
+        /// Repository for the obervation controller
+        /// </summary>
+        private IRepository<object> repository;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="repository"></param>
+        public ObservationController(IRepository<object> repository)
+        {
+            this.repository = repository;
+        }
+
+        // GET: api/Observation
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Observation/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Observation
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Observation/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Observation/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Observatory/Models/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Observatory.Models
+{
+    public interface IRepository<T>
+    {
+        Task<int> Create(T obj);
+        Task<T> Get(int key);
+        Task Update(T obj);
+        Task Delete(int key);
+    }
+}
